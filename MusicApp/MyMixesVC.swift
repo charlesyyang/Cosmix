dc114b9a45a899b4f06f516407f8effdc96a0f2c
Charles Yang <charlesyy77@gmail.com>
2019-11-09 13:26:26 -0800

    MyMixesVC created

---
+++ MusicApp/MyMixesVC.swift
@@ -0,0 +1,34 @@
+//
+//  MyMixesVC.swift
+//  MusicApp
+//
+//  Created by Charles Yang on 11/9/19.
+//  Copyright © 2019 shaina. All rights reserved.
+//
+
+import UIKit
+
+class MyMixesVC: UIViewController {
+
+    @IBOutlet weak var MixesTableView: UITableView!
+    
+    @IBOutlet weak var AddNewMixButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
