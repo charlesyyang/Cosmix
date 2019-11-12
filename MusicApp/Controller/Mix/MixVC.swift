b4dae53dc5c656c387b23e8e5528cd9bd4fb5df8
Charles Yang <charlesyy77@gmail.com>
2019-11-11 16:15:57 -0800

    fixed bug

--- MusicApp/Controller/Mix/MixVC.swift
+++ MusicApp/Controller/Mix/MixVC.swift
@@ -9,8 +9,44 @@
 import Foundation
 import UIKit
 
-class MixVC: UIViewController, UITableView {
+class MixVC: UIViewController, UITableViewDelegate{
     
-    
-    
+    var mixes = [Mix]()
+        var selectedMix = Mix?.self
+        
+        @IBOutlet weak var MixesTableView: UITableView!
+        
+        
+        @IBOutlet weak var AddMixButton: UIBarButtonItem!
+        
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+
+            // Do any additional setup after loading the view.
+        }
+        
+
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return mixes.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            if let cell = tableView.dequeueReusableCell(withIdentifier: "mixTableCell") as? MyMixTableViewCell {
+                let mix = mixes[indexPath.row]
+                cell.mixID.text = mix.id
+                cell.mixName.text = mix.name
+                return cell
+            }
+            
+            return UITableViewCell()
+        }
+        
+    //    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //        selectedMix = mixes[indexPath.row]
+    //        performSegue(withIdentifier: "to", sender: self)
+    //    }
 }
+    
+
