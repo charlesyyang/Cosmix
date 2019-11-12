8dd53b1df78dfeb05a5bfc4cb89e7aab6a13cee7
Charles Yang <charlesyy77@gmail.com>
2019-11-11 16:06:56 -0800

    add MixVC

---
+++ MusicApp/Controller/Mix/MixVC.swift
@@ -0,0 +1,16 @@
+//
+//  MixVC.swift
+//  MusicApp
+//
+//  Created by Charles Yang on 11/11/19.
+//  Copyright © 2019 shaina. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MixVC: UIViewController, UITableView {
+    
+    
+    
+}
