5d5869e07bd43fcb44da1935370745a46028a165
Charles Yang <charlesyy77@gmail.com>
2019-11-16 15:01:23 -0800

    a lot of things

--- MusicApp/Controller/CreateVC.swift
+++ MusicApp/Controller/CreateVC.swift
@@ -12,5 +12,21 @@ import Alamofire
 
 
 class CreateVC: UIViewController {
-
+    
+    @IBOutlet weak var SpaceID: UITextField!
+    
+    @IBOutlet weak var SpacePassword: UITextField!
+    
+    @IBAction func CreateSpaceButton(_ sender: Any) {
+        let parameters = ["id": SpaceID.text]
+        guard let url = URL(string: "https://us-central1-streamline-5ab87.cloudfunctions.net/new_party") else {
+            return
+        }
+        AF.request(url, method: .post, parameters: parameters)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
 }
