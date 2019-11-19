9d8f45e8281241f6bba2d97cacbf70a51ed10620
Shaina <shaina.sierra@gmail.com>
2019-11-18 17:13:39 -0800

    implement post playlist and catch errors for invalid IDs

--- MusicApp/Controller/CreateVC.swift
+++ MusicApp/Controller/CreateVC.swift
@@ -12,21 +12,90 @@ import Alamofire
 
 
 class CreateVC: UIViewController {
-    
     @IBOutlet weak var SpaceID: UITextField!
     
     @IBOutlet weak var SpacePassword: UITextField!
     
     @IBAction func CreateSpaceButton(_ sender: Any) {
-        let parameters = ["id": SpaceID.text]
-        guard let url = URL(string: "https://us-central1-streamline-5ab87.cloudfunctions.net/new_party") else {
-            return
+        //if space ID is empty, generate alert
+        if SpaceID.text == "" {
+            self.generateAlert(title: "No Space ID Provided", message: "Please enter a Space ID", alertActionTitle: "OK")
+        } else {
+            let parameters = ["id": SpaceID.text!]
+                   guard let url = URL(string: "https://us-central1-streamline-5ab87.cloudfunctions.net/check_party") else {
+                           return
+                       }
+            
+                       //check if space ID already exists in database
+                       AF.request(url, parameters: parameters).validate().responseJSON { response in
+                       switch response.result {
+                       case .success(let value):
+                           print("running party success")
+                           if let JSON = value as? [String: Any] {
+                               let idsuccess = JSON["result"] as! Bool
+                               if idsuccess {
+                                self.generateAlert(title: "Space ID Taken", message: "Please try another Space ID", alertActionTitle: "OK")
+                               } else {
+                                   guard let url = URL(string: "https://us-central1-streamline-5ab87.cloudfunctions.net/new_party") else {
+                                       return
+                                   }
+                                   AF.request(url, method: .post, parameters: parameters, encoder: JSONParameterEncoder.default).response { response in
+                                       debugPrint(response) }
+                                   self.performSegue(withIdentifier: "toMix", sender: self)
+                                
+                                }
+                           }
+                       case .failure(let error):
+                           self.generateAlert(title: "Space ID Error", message: "Please try another Space ID", alertActionTitle: "OK")
+                       }
+                   }
         }
-        AF.request(url, method: .post, parameters: parameters)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    func generateAlert(title: String, message: String, alertActionTitle: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: alertActionTitle, style: .default))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    /// Checks if party exists.
+    /// If party exists, return true.
+    /// Else, return false.
+    /// - Parameter param: ["id": party id]
+    func partyExists(param: [String: String]) -> Bool {
+        print("running party exists")
+        var partyExists = false
+        guard let url = URL(string: "https://us-central1-streamline-5ab87.cloudfunctions.net/check_party") else {
+                return false
+            }
+            
+            AF.request(url, parameters: param).validate().responseJSON { response in
+            print(response.request)  // original URL request
+            print(response.response) // URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            
+            switch response.result {
+            case .success(let value):
+                print("running party success")
+                if let JSON = value as? [String: Any] {
+                    let idsuccess = JSON["result"] as! Bool
+                    if idsuccess {
+                        partyExists = true
+                    } else {
+                        partyExists = false
+                    }
+                }
+                
+            case .failure(let error):
+                partyExists = false
+            }
+        }
+        return partyExists
+    }
 }
--- MusicApp/Controller/JoinVC.swift
+++ MusicApp/Controller/JoinVC.swift
@@ -21,38 +21,14 @@ class JoinVC: UIViewController {
     @IBOutlet weak var JoinButton: UIButton!
     
     
-    
     @IBAction func JoinButtonPressed(_ sender: Any) {
-        var param = ["id": SpaceID.text]
-        
-        guard let url = URL(string: "https://us-central1-streamline-5ab87.cloudfunctions.net/check_party") else {
-            return
-        }
-        
-        AF.request(url, parameters: param).validate().responseJSON { response in
-        print(response.request)  // original URL request
-        print(response.response) // URL response
-        print(response.data)     // server data
-        print(response.result)   // result of response serialization
-        
-        switch response.result {
-        case .success(let value) :
-            
-            if let JSON = value as? [String: Any] {
-                let idsuccess = JSON["result"] as! Bool
-                if idsuccess {
-                    self.performSegue(withIdentifier: "JoinSuccess", sender: self)
-                } else {
-                    self.generateAlert()
-                }
-            }
-            
-        case .failure(let error):
+        if SpaceID.text == "" {
             self.generateAlert()
+        } else {
+            var param = ["id": SpaceID.text!]
+            partyExists(param: param)
         }
     }
-    
-}
     
     func generateAlert() {
         let alertController = UIAlertController(title: "Invalid ID", message: "Please enter a valid ID", preferredStyle: .alert)
@@ -65,7 +41,40 @@ class JoinVC: UIViewController {
         
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+    }
+    
+    /// Checks if party exists.
+    /// If party exists, segues to next page.
+    /// Else, generate an alert for user to enter valid party id.
+    /// - Parameter param: ["id": party id]
+    func partyExists(param: [String: String]) {
+        guard let url = URL(string: "https://us-central1-streamline-5ab87.cloudfunctions.net/check_party") else {
+                return
+            }
+            
+            AF.request(url, parameters: param).validate().responseJSON { response in
+            print(response.request)  // original URL request
+            print(response.response) // URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            
+            switch response.result {
+            case .success(let value) :
+                
+                if let JSON = value as? [String: Any] {
+                    let idsuccess = JSON["result"] as! Bool
+                    if idsuccess {
+                        self.performSegue(withIdentifier: "JoinSuccess", sender: self)
+                    } else {
+                        self.generateAlert()
+                    }
+                }
+                
+            case .failure(let error):
+                self.generateAlert()
+            }
+        }
     }
 }
