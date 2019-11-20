03f8914a67cf978f4695e14d57d483d65f066b18
Charles Yang <charlesyy77@gmail.com>
2019-11-19 22:16:19 -0800

    more spotify progress

--- MusicApp/Controller/AddSpotifyVC.swift
+++ MusicApp/Controller/AddSpotifyVC.swift
@@ -13,14 +13,23 @@ class AddSpotifyVC: UIViewController {
 
     
     let SpotifyClientID = "2fd46a7902e043e4bcb8ccda3d1381b2"
-    let SpotifyRedirectURL = URL(string: "http://com.example.streamline/callback")!
+    let SpotifyRedirectURI = URL(string: "http://com.example.streamline/callback")!
     
- //   lazy var configuration = SPTConfiguration(
- //       clientID = Spoti)
-    
-    
-    
-    
+    lazy var configuration = SPTConfiguration(
+      clientID: SpotifyClientID,
+      redirectURL: SpotifyRedirectURI
+    )
     
     
+//    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+//      let parameters = appRemote.authorizationParameters(from: url);
+//
+//            if let access_token = parameters?[SPTAppRemoteAccessTokenKey] {
+//                appRemote.connectionParameters.accessToken = access_token
+//                self.accessToken = access_token
+//            } else if let error_description = parameters?[SPTAppRemoteErrorDescriptionKey] {
+//                // Show the error
+//            }
+//      return true
+//    }
 }
